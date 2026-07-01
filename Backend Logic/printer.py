df1b03de9e6d5314afd3cafb5a7f9f8df2a3e79e
ivanuary <248451981+ivanuary@users.noreply.github.com>
2026-07-01 15:29:16 +0800

    Cleaned JS layout, separated exact and related matches using a dictionary, added prices to the products, added opening and closing time as well as python function to add an open status for the stores

--- Backend Logic/printer.py
+++ Backend Logic/printer.py
@@ -1,23 +1,44 @@
-def print_stores_with_products(list_stores_product:list):
-    for i, match_list in enumerate(list_stores_product):
-        if i == 0:
-            print("EXACT MATCHES:")
-            print("--------------")
+def print_stores_with_products(list_stores_product:dict):
+    print("Exact Matches: ")
+    print("-----------------")
+    for store in list_stores_product["exact_matches"]:
+        print(f"Store Name: {store['store_name'].title()}")
+        print(f"Store Address: {store['store_address'].title()}")
+        print(f"Distance: {store['distance']:0.3f}km away")
+
+        if store["is_open"] == True:
+            print("Status: Open")
         else:
-            print("RELATED MATCHES:")
-            print("----------------")
+            print("Status: Closed")
+        
+        print("Products")
+        for product in store['store_products']:
+            print(f" > {product.title()}")
 
-        for i, store in enumerate(match_list):
-            print(f"Store Name: {store['store_name'].title()}")
-            print(f"Store Address: {store['store_address'].title()}")
-            print(f"Distance: {store['distance']:0.3f}km away")
-
-            print("Products")
-            for product in store['store_products']:
-                print(f" > {product.title()}")
-
-            print("Payment Methods")
-            for payment in store['payment_methods']:
-                print(f" > {payment.title()}")
+        print("Payment Methods")
+        for payment in store['payment_methods']:
+            print(f" > {payment.title()}")
             
-            print("\n")
+        print("\n")
+
+    print("Related Matches: ")
+    print("-----------------")
+    for store in list_stores_product["related_matches"]:
+        print(f"Store Name: {store['store_name'].title()}")
+        print(f"Store Address: {store['store_address'].title()}")
+        print(f"Distance: {store['distance']:0.3f}km away")
+        
+        if store["is_open"] == True:
+            print("Status: Open")
+        else:
+            print("Status: Closed")
+
+        print("Products")
+        for product, price in store['store_products'].items():
+            print(f" > {product}: PHP {price}")
+
+        print("Payment Methods")
+        for payment in store['payment_methods']:
+            print(f" > {payment.title()}")
+            
+        print("\n")
--- Backend Logic/search_service.py
+++ Backend Logic/search_service.py
@@ -1,29 +1,23 @@
 import navigation
+import time_logic
 
-def sort_by_distance(stores_with_product:list, user_coordinates:tuple):
-    # Calculates distance per exact match list and related match list
-    stores_by_match_with_distance = []
-    for match_list in stores_with_product:
-        stores_with_distance = navigation.calculate_distance(user_coordinates, match_list)
-        stores_by_match_with_distance.append(stores_with_distance)
+def sort_by_distance(stores_with_product:dict, user_coordinates:tuple):
+    stores_with_product["exact_matches"] = navigation.calculate_distance(user_coordinates, stores_with_product["exact_matches"])
+    stores_with_product["related_matches"] = navigation.calculate_distance(user_coordinates, stores_with_product["related_matches"])
 
-    sorted_stores_by_match = []
-    for match_list in stores_by_match_with_distance:
-        sorted_stores = sorted(match_list, key=lambda store: store['distance'])
-        sorted_stores_by_match.append(sorted_stores)
+    stores_with_product["exact_matches"] = sorted(stores_with_product["exact_matches"], key=lambda store: store['distance'])
+    stores_with_product["related_matches"] = sorted(stores_with_product["related_matches"], key=lambda store: store['distance'])
 
-    return sorted_stores_by_match
+    return stores_with_product
 
 def search_product_via_stores(search_key:str, stores_dict:dict, user_coordinates:tuple):
     search_key = search_key.strip().lower()
-    stores_list = stores_dict["store"]
-
-    #First list will be exact matches, 2nd list would be for related matches
+    stores_list = time_logic.is_store_open(stores_dict["store"])
 
     exact_matches = []
     related_matches = []
 
-    for i, store in enumerate(stores_list):
+    for store in stores_list:
         products = [prod.strip().lower() for prod in store['store_products']]
 
         if search_key in products:
@@ -33,9 +27,15 @@ def search_product_via_stores(search_key:str, stores_dict:dict, user_coordinates
         for product in products:
             if search_key in product:
                 related_matches.append(store)
-
-    stores_with_product = [exact_matches, related_matches]
+                break
     
+    stores_with_product = {"exact_matches":exact_matches, "related_matches":related_matches}
+    
+    if len(stores_with_product["exact_matches"]) == 0 and len(stores_with_product["related_matches"]) == 0:
+        stores_with_product["has_results"] = False
+    else:
+        stores_with_product["has_results"] = True
+
     sorted_stores = sort_by_distance(stores_with_product, user_coordinates)
 
     return sorted_stores
